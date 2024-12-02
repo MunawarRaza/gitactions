1abe16218705c9dc139e06e7a654175ddf687611
Muhammad Munawar aza <60610263+MunawarRaza@users.noreply.github.com>
2024-12-02 15:56:02 +0500

    deploy chat

--- docker/aws/scripts/deploy.sh
+++ docker/aws/scripts/deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-docker image rm -f 590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous || true
+echo '# Remove Old Image #'
+# docker image rm -f 590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous || true
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 590183784162.dkr.ecr.us-east-1.amazonaws.com
-docker pull 590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous:latest
-docker container rm -f main_ai_chat_luminous || true
+docker pull 590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous:main
+# docker container rm -f main_ai_chat_luminous || true
 docker run -dt \
 	  --name ${ENVIRONMENT}_ai_chat_luminous \
-	  -p ${PORT_AI_CHAT}:80
+	  -p ${PORT_AI_CHAT}:80 \
 	  #-p 8080:80 
-	   590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous:latest
+	   590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous:main
 docker rmi $(docker images -f "dangling=true" -q)
 
