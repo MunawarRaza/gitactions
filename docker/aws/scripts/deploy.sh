88448d5da4cbd58fecb4c47718a3027698fcca7f
Muhammad Munawar aza <60610263+MunawarRaza@users.noreply.github.com>
2024-12-02 17:10:31 +0500

    Update deploy.sh

--- docker/aws/scripts/deploy.sh
+++ docker/aws/scripts/deploy.sh
@@ -7,7 +7,7 @@ docker pull 590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous:m
 # docker container rm -f main_ai_chat_luminous || true
 docker run -dt \
 	  --name main_ai_chat_luminous \
-	  -p ${PORT_AI_CHAT}:80 \
+	  -p 8080:80 \
 	   590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous:main
 docker rmi $(docker images -f "dangling=true" -q)
 
