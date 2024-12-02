561dcbe3f705ab41ed5604be401bdc1e6205dafa
Muhammad Munawar aza <60610263+MunawarRaza@users.noreply.github.com>
2024-12-02 16:00:02 +0500

    deploy chat

--- docker/aws/scripts/deploy.sh
+++ docker/aws/scripts/deploy.sh
@@ -6,9 +6,8 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 docker pull 590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous:main
 # docker container rm -f main_ai_chat_luminous || true
 docker run -dt \
-	  --name ${ENVIRONMENT}_ai_chat_luminous \
+	  --name main_ai_chat_luminous \
 	  -p ${PORT_AI_CHAT}:80 \
-	  #-p 8080:80 
 	   590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous:main
 docker rmi $(docker images -f "dangling=true" -q)
 
