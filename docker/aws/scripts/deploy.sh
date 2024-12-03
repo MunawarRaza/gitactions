80a2b43a4db560fe9ce8ffd5d1d9b68eacb9546e
Muhammad Munawar aza <60610263+MunawarRaza@users.noreply.github.com>
2024-12-03 10:23:33 +0500

    deploy chat

--- docker/aws/scripts/deploy.sh
+++ docker/aws/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo '# Remove Old Image #'
-# docker image rm -f 590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous || true
+docker image rm -f 590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous || true
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 590183784162.dkr.ecr.us-east-1.amazonaws.com
 docker pull 590183784162.dkr.ecr.us-east-1.amazonaws.com/main_ai_chat_luminous:main
-# docker container rm -f main_ai_chat_luminous || true
+docker container rm -f main_ai_chat_luminous || true
 docker run -dt \
 	  --name main_ai_chat_luminous \
 	  -p 8080:80 \
